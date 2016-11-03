e6bc6e9958c18b16d5030af761c02ba1dfb955fd
sijie.li <lsj86@qq.com>
2016-11-03 14:42:24 +0800

    去掉supervisor，减少镜像体积

--- apps/nginx_php/startup.sh
+++ apps/nginx_php/startup.sh
@@ -3,4 +3,9 @@ set -e
 
 echo "xdebug.remote_host=$XDEBUG_HOST" > /usr/local/etc/conf.d/xdebug.ini
 
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+if [ -e "/usr/bin/supervisord" ]; then
+	/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+else
+	/usr/local/sbin/php-fpm
+	/usr/sbin/nginx -g 'daemon off;'
+fi
